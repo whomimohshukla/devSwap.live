cea5b9bcddde012ea556c41ac1d07e79a5d07a94
Mimohshukla00 <mimohshukla0001@gmail.com>
2025-08-15 14:03:50 +0530

    feedback

---
+++ server/src/models/feedBack.model.ts
@@ -0,0 +1,37 @@
+import { Schema, model, Document, Types } from "mongoose";
+
+export interface IFeedback extends Document {
+	sessionId: Types.ObjectId;
+	fromUser: Types.ObjectId;
+	toUser: Types.ObjectId;
+	rating: number;
+	comment?: string;
+}
+
+const feedbackSchema = new Schema<IFeedback>(
+	{
+		sessionId: {
+			type: Schema.Types.ObjectId,
+			ref: "Session",
+			required: true,
+			index: true,
+		},
+		fromUser: {
+			type: Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+			index: true,
+		},
+		toUser: {
+			type: Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+			index: true,
+		},
+		rating: { type: Number, min: 1, max: 5, required: true, index: true },
+		comment: String,
+	},
+	{ timestamps: true }
+);
+
+export const Feedback = model<IFeedback>("Feedback", feedbackSchema);
