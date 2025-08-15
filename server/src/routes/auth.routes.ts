35b00b71b22921f379bd6eac0e64849b78670ef7
Mimohshukla00 <mimohshukla0001@gmail.com>
2025-08-15 14:05:11 +0530

    auth routes

---
+++ server/src/routes/auth.routes.ts
@@ -0,0 +1,10 @@
+// import { Router } from "express";
+// // import { login, signup } from "../controllers/authController";
+// import { strictLimiter } from "../middleware/rateLimiter";
+
+// const router = Router();
+
+// router.post("/login", strictLimiter, login);
+// router.post("/signup", strictLimiter, signup);
+
+// export default router;
