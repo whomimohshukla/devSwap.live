0f8f21009fa60d7ebed36ff57dceb8053a3a5186
Mimohshukla00 <mimohshukla0001@gmail.com>
2025-08-17 00:39:43 +0530

    installation of vite react bundler and tailwind

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	
+	return (
+		<>
+			<div className='bg-blue-800 text-center p-4 m-4 text-red-500'>
+				<h1 className='text-3xl font-bold'>
+					Welcome to <span className='text-blue-600'>DevSwap.live</span>
+				</h1>
+				<p className='text-xl'>
+					This is a sample application to demonstrate the use of WebSockets
+					for real-time communication between the server and the client.
+				</p>
+				<p className='text-xl'>
+					The server is running at{" "}
+					<span className='text-blue-600'>ws://localhost:5000</span> and
+					the client is running at{" "}
+					<span className='text-blue-600'>http://localhost:3000</span>.
+				</p>
+				<p className='text-xl'>
+					To start the server, run{" "}
+					<span className='text-blue-600'>npm run dev</span> in the server
+					directory.
+				</p>
+				<p className='text-xl'>
+					Once the server is running, open a new terminal window and run{" "}
+					<span className='text-blue-600'>npm run dev</span> in the client
+					directory.
+				</p>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
